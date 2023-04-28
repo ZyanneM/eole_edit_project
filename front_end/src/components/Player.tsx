342594fe72b5ca39be82a7536e10422d0ae85672
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-04-28 09:52:25 +0200

    add player

--- front_end/src/components/Player.tsx
+++ front_end/src/components/Player.tsx
@@ -1,35 +1,61 @@
-import React , {useState, useEffect} from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
+import React , {useState, useEffect, useRef} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-const Player = (props:any) => {
 
-    // const [videoUrls, setVideoUrls] = useState<Array<any>>([]);
 
-    // useEffect(() => {
-    //   const fetchVideoUrls = async () => {
-    //     const urls = await Promise.all(props.videos.map(async (video) => {
-    //       const response = await axios.get(`http://localhost:3000/video/${video}`, { responseType: 'blob' });
-    //       const url = URL.createObjectURL(response.data);
-    //       return { name: video, url };
-    //     }));
-    //     setVideoUrls(urls);
-    //   };
-    //   fetchVideoUrls();
-    // }, [props.videos]);
+type PlayerComponentProps = {
+  filename : string
+
+}
+
+const Player = ({filename}:PlayerComponentProps) => {
+
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const [ isPlaying, setIsPlaying ] = useState<boolean>(true);
+
+
+
+function handlePlay() {
+  setIsPlaying(true);
+  if(videoRef.current){
+    console.log('play');
+    videoRef.current.play();
+
+    // videoRef.current.load()
+}
+
+
+}
+
+function handlePause() {
+
+  setIsPlaying(false);
+  if(videoRef.current){
+    console.log('pause');
+    videoRef.current.pause();
+
+    // videoRef.current.load()
+}
+
+
+}
 
     return (
         <>
-            {props.videos.map((video) => {
-               return <li
-               key={uuidv4()}
-               id={uuidv4()}>
-                <a href="#">{video}</a>
-                <video autoPlay="autoplay" muted="muted" loop="loop" playsInline="playsinline" preload="metadata" data-aos="fade-up" controls width="280">
-                    <source src={`http://localhost:3000/uploads/processed/${video}`} type="video/mp4"></source>
-                </video>
-                </li>
-            })}
+                { filename && 
+                <>
+                <div className='video-container'>
+                  <video ref={videoRef} id="video-player" muted="muted" loop="loop" playsInline="playsinline" preload="metadata" data-aos="fade-up" autoPlay width="480" height="auto">
+                      <source src={`http://localhost:3000/files/${filename}`} type="video/mp4" />
+                  </video>
+                </div>
+                {isPlaying ?
+                <button onClick={() => handlePause()}>Pause</button>
+                : <button onClick={() => handlePlay()}>Play</button>}
+                <button type="button" className="btn-close btn-close-white" aria-label="Close"></button>
+                </>
+                }
         </>
     );
 };
--- front_end/src/components/VideosList.tsx
+++ front_end/src/components/VideosList.tsx
@@ -9,12 +9,15 @@ const VideosList = () => {
 
     const [ videos, setVideos ] = useState<Array<any>>([]);
 
+    const [ selectedVideo, setSelectedVideo ] = useState<string>('');
+    const [ isOpen, setIsOpen ] = useState<boolean>(true);
+
     useEffect(() => {
         async function getVideos() {
           try {
             const response = await axios.get('http://localhost:3000/files');
-            const data = response.data;
-            setVideos(prevVideos => [...prevVideos, ...data]);
+            const data = response.data; 
+            setVideos([...data]);
           } catch (error) {
             console.error(error);
           }
@@ -22,17 +25,42 @@ const VideosList = () => {
         getVideos();
       }, []);
 
+      function handlePlay (video:string){
+        setSelectedVideo(video)
+        setIsOpen(true)
+      }
+      // console.log(selectedVideo);
 
+      function handleClose() {
+        setIsOpen(false)
+      
+      }
 
     return (
-        <div>
+        <div className='video-section'>
             <h1 className='files-title'>🎞️ Videos Gallery</h1>
-            <ul>
-             <Player 
-             videos = {videos}/>
-            </ul>
+              {videos.map((video) =>
+                <button
+                key={uuidv4()}
+                className='single-video-btn'
+                onClick={ () => handlePlay(video)}>
+                  {video}
+                  </button>
+              )}
+              {selectedVideo && isOpen ? 
+              <>
+              <Player
+              filename={selectedVideo}
+              />
+              <button onClick={() => handleClose()} type="button" className="btn-close btn-close-white" aria-label="Close"></button>
+              </> :
+              ""}
         </div>
     );
 };
 
 export default VideosList;
+
+{/* <Player
+key={uuidv4()} 
+video = {video}/> */}
